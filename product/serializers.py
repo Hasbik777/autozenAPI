b827fad66f254a4cebc6bcc3624529a5ae3f34e3
sanzhar <sanjar.turumov@gmail.com>
2023-10-18 00:58:06 +0600

    fix product

--- product/serializers.py
+++ product/serializers.py
@@ -8,15 +8,6 @@ class ProductListSerializer(serializers.ModelSerializer):
         fields = ('title', 'price', 'image', 'stock')
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    owner_email = serializers.ReadOnlyField(source='owner.email')
-    owner = serializers.ReadOnlyField(source='owner.id')
-
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-
 class ProductRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductRequest
