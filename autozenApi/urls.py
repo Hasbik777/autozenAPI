545bac95022b7f99f32ccf709c191dec19396204
sanzhar <sanjar.turumov@gmail.com>
2023-10-22 16:15:30 +0600

    req.txt

--- autozenApi/urls.py
+++ autozenApi/urls.py
@@ -47,7 +47,7 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('account.urls')),
-    path('api/v1/orders/', include('order.urls')),
+    # path('api/v1/orders/', include('order.urls')),
     path('api/v1/', include(router.urls)),
 ]
 
